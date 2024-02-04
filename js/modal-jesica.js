edd83a5187cc816bfdef579958f1935e401160b7
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2024-02-04 13:37:59 +0200

    Update modal-jesica.js

--- js/modal-jesica.js
+++ js/modal-jesica.js
@@ -1,1 +1,14 @@
-
+(() => {
+    const refs = {
+      openInfo: document.querySelector("[data-jesica]"),
+      closeInfo: document.querySelector("[data-jesica-close]"),
+      modal: document.querySelector("[data-photo-jesica]"),
+    };
+  
+    refs.openInfo.addEventListener("click", toggleModal);
+    refs.closeInfo.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
