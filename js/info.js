c6171dca12a1644e2dd3ecfe84396e963c22a57d
lika-ruby <mary.havrylova.2011@gmail.com>
2024-02-04 12:40:59 +0200

    Makes first modal

--- js/info.js
+++ js/info.js
@@ -2,11 +2,11 @@
   const refs = {
     openInfo: document.querySelector("[data-info]"),
     closeInfo: document.querySelector("[data-info-close]"),
-    modal: document.querySelector("[data-modal]"),
+    modal: document.querySelector("[data-photo]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openInfo.addEventListener("click", toggleModal);
+  refs.closeInfo.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
